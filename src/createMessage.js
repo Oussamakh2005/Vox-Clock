0a2191d8cf715c04a1ee129a07d8c73fc41cb2bc
Oussama <khelefoussama2005@gmail.com>
2025-02-16 18:07:28 +0100

    now bot can parse time to text

---
+++ src/createMessage.js
@@ -0,0 +1,23 @@
+import data from "./data.js";
+
+const createMessage = (time) => {
+    let message = "";
+    const morningNoon = (time.amPm == "am")? "in the morning" : "in the noon"
+    switch(time.minutes){
+        case 0 : 
+        message = `Its ${data[time.hour]} o'clock`;
+        break;
+        case 15 : 
+        message = `Its a quarter past ${data[time.hour]}`;
+        break;
+        case 30 : 
+        message = `Its a half past ${data[time.hour]}`;
+        break;
+        default :
+        message = `The current time is ${data[time.hour]} ${data[time.minutes]}`;
+        break;
+    }
+    return message+` in the ${morningNoon}`
+}
+
+export default createMessage;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { Telegraf } from "telegraf";
 import getTime from "./getTime.js";
+import createMessage from "./createMessage.js";
 
 dotenv.config();
 
@@ -8,8 +9,8 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_KEY);
 
 bot.start((ctx) => ctx.reply(`Hello I'm Vox Clock ,just send \\time and I will replay with audio contain talking clock`));
 bot.hears('/time',(ctx) => {
-    const time = getTime();
-    ctx.reply(`${time.hour} : ${time.minutes} ${time.amPm}`)
+    const timeText = createMessage(getTime());
+    ctx.reply(timeText);
 })
 
 bot.launch(() => {
